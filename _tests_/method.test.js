9057d9933dafae62142acb28cc47b465ebafca4b
Nick Hennessy <njhnny@gmail.com>
2020-11-19 07:04:28 -0800

    add mess and broken code

---
+++ _tests_/method.test.js
@@ -0,0 +1,10 @@
+import Triangle from './src/method.js';
+
+describe('Method', () => {
+  
+    test('should multiply user age by planet multipliers', () => {
+      const method = new Method(28, []);
+      //expectmethod;
+      
+    });
+});
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+import Method from './src/method.js';
+
+$(document).ready(function() {
+  $("form#form").submit(function(event) {
+    event.preventDefault();
+    let userInput = $("input#submission").val();
+    let agesObject = new Method(userInput);
+    console.log(agesObject)
+    $("p").text(agesObject.toString);
+  });
+});
