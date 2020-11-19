9731a2d8a3204f649301b8c2531805a42fe3bfce
Nick Hennessy <njhnny@gmail.com>
2020-11-19 03:15:00 -0800

    create method class with age multiplier

---
+++ src/method.js
@@ -0,0 +1,17 @@
+export default class Method {
+  constructor() {
+    this.age = age;
+    this.spaceAges = [];
+  }
+
+  ageCalculator() {
+    let ageMultipliers = [.24, .62, 1.88, 11.86];
+    let variable = this.spaceAges;
+    let age2 = age;
+    ageMultipliers.forEach(myFunction)
+    function myFunction(multiplier) {
+      variable.push(age2*multiplier);
+  }
+  return this.spaceAges;
+  }
+}
