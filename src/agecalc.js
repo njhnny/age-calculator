80e2d911ae37e94acb3e2d754f4117ef9e3d8d53
Nick Hennessy <njhnny@gmail.com>
2020-11-30 01:44:24 -0800

    add the rest of the life expectancy functions

--- src/agecalc.js
+++ src/agecalc.js
@@ -14,13 +14,33 @@ export default class Agecalc {
   venusAge() {
     return Math.round(this.spaceAges / .62);
   }
+  venusLeft() {
+    let thisAge = Math.round(this.spaceAges / .62);
+    let expectancy = Math.round(85 / .62);
+    return expectancy - thisAge;
+  }
   marsAge() {
     return Math.round(this.spaceAges / 1.88);
+  }
+  marsLeft() {
+    let thisAge = Math.round(this.spaceAges / 1.88);
+    let expectancy = Math.round(85 / 1.88);
+    return expectancy - thisAge;
   }
   jupiterAge() {
     return Math.round(this.spaceAges / 11.86);
   }
+  jupiterLeft() {
+    let thisAge = Math.round(this.spaceAges / 11.86);
+    let expectancy = Math.round(85 / 11.86);
+    return expectancy - thisAge;
+  }
   saturnAge() {
     return Math.round(this.spaceAges / 29.46);
+  }
+  saturnLeft() {
+    let thisAge = Math.round(this.spaceAges / 29.46);
+    let expectancy = Math.round(85 / 29.46);
+    return expectancy - thisAge;
   }
 }
