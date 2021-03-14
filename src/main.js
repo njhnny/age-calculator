f15d39a568cc6b7e598b7682558830e5198e738c
Nick Hennessy <njhnny@gmail.com>
2021-03-13 20:04:47 -0800

    remove document ready function and console log

--- src/main.js
+++ src/main.js
@@ -5,26 +5,23 @@ import './css/styles.css';
 
 import Agecalc from './../src/agecalc.js';
 
-$(document).ready(function() {
-  $("form#form").submit(function(event) {
-    event.preventDefault();
-    let userInput = $("input#submission").val();
-    let agesObject = new Agecalc(userInput);
-    console.log(agesObject);
-    let mercuryAge = agesObject.mercuryAge();
-    let mercuryRemain = agesObject.mercuryLeft();
-    let venusAge = agesObject.venusAge();
-    let venusRemain = agesObject.venusLeft();
-    let marsAge = agesObject.marsAge();
-    let marsRemain = agesObject.marsLeft();
-    let jupiterAge = agesObject.jupiterAge();
-    let jupiterRemain = agesObject.jupiterLeft();
-    let saturnAge = agesObject.saturnAge();
-    let saturnRemain = agesObject.saturnLeft();
-    let yearsLeft = agesObject.pastExpect();
-    $("p").append (`Your time left on Mercury is ${mercuryRemain} Mercury years <br>Your time left on Venus is ${venusRemain} Venus years <br>Your time left on Mars is ${marsRemain} Mars years <br>Your time left on Jupiter is ${jupiterRemain} Jupiter years <br>Your time left on saturn is ${saturnRemain} Saturn years <br>`);
-    $("p").append(mercuryAge);
-    $("p").append(", ", venusAge, ", ", marsAge, ", ", jupiterAge, ", ", saturnAge);
-    $("p").append(yearsLeft);
-  });
+
+$("form#form").submit(function(event) {
+  event.preventDefault();
+  let userInput = $("input#submission").val();
+  let agesObject = new Agecalc(userInput);
+  let mercuryAge = agesObject.mercuryAge();
+  let mercuryRemain = agesObject.mercuryLeft();
+  let venusAge = agesObject.venusAge();
+  let venusRemain = agesObject.venusLeft();
+  let marsAge = agesObject.marsAge();
+  let marsRemain = agesObject.marsLeft();
+  let jupiterAge = agesObject.jupiterAge();
+  let jupiterRemain = agesObject.jupiterLeft();
+  let saturnAge = agesObject.saturnAge();
+  let saturnRemain = agesObject.saturnLeft();
+  let yearsLeft = agesObject.pastExpect();
+  $("p").append (`Your time left on Mercury is ${mercuryRemain} Mercury years <br>Your time left on Venus is ${venusRemain} Venus years <br>Your time left on Mars is ${marsRemain} Mars years <br>Your time left on Jupiter is ${jupiterRemain} Jupiter years <br>Your time left on saturn is ${saturnRemain} Saturn years <br>`);
+  $("p").append(mercuryAge, ", ", venusAge, ", ", marsAge, ", ", jupiterAge, ", ", saturnAge);
+  $("p").append(yearsLeft);
 });
